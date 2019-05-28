aa44b0f269067399e3141a2c06056481f83e5e90
ElimuStuart <stuartelimu@gmail.com>
2019-05-28 10:44:15 +0300

    created post and category model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    overview = models.TextField()
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=120, unique_for_date=timestamp)
+    categories = models.ManyToManyField(Category)
+
+    class Meta:
+        ordering = ('-timestamp',)
+
+    def __str__(self):
+        return self.title
+
+    
+
+
